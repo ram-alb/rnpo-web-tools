d949c0f207f7269a503a1873126225f87875a8f2
Ramil Albakov <ramil.albakov2@gmail.com>
2022-10-26 17:46:19 +0600

    Add frontend part for network_vs_atoll app
    Frontend part displays dummy data. Later it
    will be replaced by real data.

--- network_vs_atoll/views.py
+++ network_vs_atoll/views.py
@@ -2,4 +2,42 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'network_vs_atoll/home.html')
+    diffs = {
+        'summary': [
+            {
+                'technology': 'GSM',
+                'count': 356,
+            },
+            {
+                'technology': 'WCDMA',
+                'count': 567,
+            },
+        ],
+        'GSM': {
+            'summary': [
+                {
+                    'bsc_name': 'BSC1',
+                    'count': 45,
+                },
+                {
+                    'bsc_name': 'BSC2',
+                    'count': 34,
+                }
+            ],
+            'BSC1': [],
+            'BSC2': [],
+        },
+        'WCDMA': {
+            'summary': [
+                {
+                    'rnc_name': 'RNC1',
+                    'count': 55,
+                },
+                {
+                    'rnc_name': 'RNC2',
+                    'count': 77,
+                },
+            ],
+        },
+    }
+    return render(request, 'network_vs_atoll/home.html', diffs)
